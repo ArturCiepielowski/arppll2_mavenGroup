2c1ae482f693bee72ffd87555813d4ac69f0a51a
Alicja Mojska <Alicja Mojska>
2022-04-05 17:34:50 +0200

    naprwiam

--- src/main/java/pl/sda/arppl/KlasaAla2.java
+++ src/main/java/pl/sda/arppl/KlasaAla2.java
@@ -2,7 +2,7 @@ package pl.sda.arppl;
 
 import java.util.Scanner;
 
-public class KlasaAla { public static void main(String[] args) {
+public class KlasaAla2 { public static void main(String[] args) {
 
     Scanner scanner = new Scanner(System.in);
 
